a3b55d589ff862ec377e1a68b15654af9b7e02b6
Tatyana Diadenko <hayalt08@gmail.com>
2020-03-30 14:34:21 +0300

    удаление пункта меню

--- inc/header.php
+++ inc/header.php
@@ -21,7 +21,6 @@
         <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
         </ul>
     </nav>
     </div>
